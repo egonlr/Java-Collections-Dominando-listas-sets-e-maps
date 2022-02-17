9fe41db8ae58146dee6935766416ec1d023f2582
egonlr <egonlr@hotmail.com>
2022-02-17 13:45:16 -0300

    Trabalhando com ArrayList

--- src/br/com/alura/TestandoListas.java
+++ src/br/com/alura/TestandoListas.java
@@ -1,6 +1,7 @@
 package br.com.alura;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class TestandoListas {
 
@@ -15,5 +16,32 @@ public class TestandoListas {
 		aulas.add(aula3);
 		
 		System.out.println(aulas);
+		
+		aulas.remove(0);
+		
+		System.out.println(aulas);
+		
+		for (String aula : aulas) {
+			System.out.println("Aula: " + aula);
+			
+		}
+		
+		String primeiraAula = aulas.get(0);
+		System.out.println("A primeira aula é " + primeiraAula);
+		
+		for (int i = 0; i < aulas.size(); i++) {
+			System.out.println("aula: " + aulas.get(i));
+			}
+		
+		System.out.println(aulas.size());
+		
+		aulas.forEach(aula -> {System.out.println("percorrendo : " + aula);});
+		
+		aulas.add("Aumentando nosso conhecimento");
+		System.out.println(aulas);
+		Collections.sort(aulas);
+		System.out.println("Depois de ordenado");
+		System.out.println(aulas);
 	}
+
 }
