5462052c6f9d00bf6546a8d7c89c5175bccaba2f
egonlr <egonlr@hotmail.com>
2022-02-18 13:40:53 -0300

    Relacionamentos com coleções

---
+++ src/br/com/alura/TestaCurso.java
@@ -0,0 +1,18 @@
+package br.com.alura;
+
+
+public class TestaCurso {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Curso javaColecoes = new Curso("Dominando as coleções do Java", "Paulo Silveira");
+
+		javaColecoes.adiciona(new Aula("Trabalhando com ArrayList", 21));
+		javaColecoes.adiciona(new Aula("Criando uma Aula", 20));
+		javaColecoes.adiciona(new Aula("Modelando com coleções", 22));
+		
+		System.out.println(javaColecoes.getAulas());
+
+	}
+
+}
