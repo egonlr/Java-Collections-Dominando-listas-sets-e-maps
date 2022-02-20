340ed1cbddded3035480b0f62b3eaafcd99f36d9
egonlr <egonlr@hotmail.com>
2022-02-19 21:04:36 -0300

    Aplicando o Set no modelo

---
+++ src/br/com/alura/Aluno.java
@@ -0,0 +1,33 @@
+package br.com.alura;
+
+import java.util.Collections;
+import java.util.Set;
+
+public class Aluno {
+	
+	private String nome;
+	private int numeroMatricula;
+
+	public Aluno(String nome, int numeroMatricula) {
+		this.nome = nome;
+		this.numeroMatricula = numeroMatricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+	
+	@Override
+	public String toString() {
+
+		return "[Aluno: " + this.nome + ", matrícula: " + this.numeroMatricula + "]";
+	}
+
+
+	
+
+}
